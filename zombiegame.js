26bad10309d8e3254340d896f0f070c65f6d9e57
Mark Kazanski <markkazanski@gmail.com>
2017-11-27 10:37:40 -0600

    gitignore again

---
+++ zombiegame.js
@@ -0,0 +1,70 @@
+// INSTRUCTIONS: Build a command-line based zombie fighting game. 
+// =========================================================================================================
+
+// In this game, you and a zombie will each be given a certain amount of health. (Perhaps: You 70, Zombie 15).
+
+// For each round, you will be asked to guess a random number between 1 and 5.
+// If your guess matches the random number of the Zombie -- you inflict a random amount of damage between 1 and 5. 
+// If you guess does not match the random number of the Zombie -- the Zombie inflicts a random amount of damage to you between 1 and 5.
+// Each round the zombie is given a new random number and you must guess again. 
+
+// The game ends when you or the zombie gets to 0 health. 
+
+// Note: You should use the inquirer package to take in user commands.
+// Major Warning: inquirer's prompt function is "asynchronous", which means that the majority of your game logic will need to be inside the .then() function for your propmt. 
+
+// ===========================================================================================================
+/*
+1. Generate user health and zombie health
+    Zombie number to guess and damage
+2. Get user guess
+    Compare to zombie number, attack or take damage
+3. Check for game over
+4. Generate next zombie. 
+5. Show gameover. 
+*/
+
+var inquirer = require("inquirer");
+
+var questions = {};
+
+var player = {
+    health:100,
+    strength:10,
+    zombiesKilled:0,
+    guess:undefined,
+    takeDamage:function(){
+        player.health -= zombie.strength;
+    }
+};
+
+var zombie = {
+    health:randomIntFromInterval(10,25),
+    strength:randomIntFromInterval(5,20),
+    secretNumber:randomIntFromInterval(1,5),
+    takeDamage:function(){
+        zombie.health -= player.strength;
+    }
+};
+
+while(player.health > 0){ //check player is alive
+    inquirer.prompt(questions).then(function(inquirerResponse){
+        //inquirerResponse.name;
+    });
+}
+
+function initGame(){
+
+}
+
+function nextZombie(){
+    //check roll
+    //check gameover
+   if( zombie.secretNumber === player.guess ){
+        zombie.takeDamage();
+   }
+}
+
+function randomIntFromInterval(min,max){ //get random number
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
